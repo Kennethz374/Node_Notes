c11d046942b70a032a60f78455a00e9308473c83
Kenneth <Kennethzhang374@gmail.com>
2020-04-23 00:01:50 -0700

    export expression without function name

--- HowNodeWorks/test-module-1.js
+++ HowNodeWorks/test-module-1.js
@@ -13,3 +13,17 @@
 // }
 
 // module.exports = Calculator;
+
+module.exports = class {
+  add(a, b) {
+    return a + b;
+  }
+
+  multiply(a, b) {
+    return a * b;
+  }
+
+  divide(a, b) {
+    return a / b;
+  }
+};
