7235505ce781d77ca88829833ca4ba24e15e8fca
Kenneth <Kennethzhang374@gmail.com>
2020-04-23 00:06:52 -0700

    use Obj destructuring for require function

--- HowNodeWorks/modules.js
+++ HowNodeWorks/modules.js
@@ -1,7 +1,15 @@
 // console.log(arguments); // everything is wrap in wrapper function
 // console.log(require("module").wrapper);
 
+// module.exports
 const C = require("./test-module-1");
 const calc1 = new C();
 
 console.log(calc1.add(2, 5));
+
+//exports.xxx
+// const calc2 = require("./test-module-2");
+//obj destructuring
+const { add, multiply, divide } = require("./test-module-2");
+console.log(add(2, 5));
+console.log(multiply(2, 5));
