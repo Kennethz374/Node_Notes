b65e80e4b930190cfb91c8ad82afe701c9e87433
Kenneth <Kennethzhang374@gmail.com>
2020-04-24 00:42:52 -0700

    asyn/await examples

--- Asyn_Promises/index.js
+++ Asyn_Promises/index.js
@@ -54,23 +54,41 @@ const writeFilePro = (file, data) => {
     });
   });
 };
-
-readFilePro(`${__dirname}/dog.txt`)
-  .then(data => {
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`bread: ${data}`);
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`); //the return will return a promise
-  })
-  .then(res => {
+
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
     console.log(res.body.message);
-    return writeFilePro(`dog-img.txt`, res.body.message);
-    // fs.writeFile(`dog-img.txt`, res.body.message, err => {
-    //   if (err) return console.log(err.message);
-    //   console.log(`random image file saved`);
-    // });
-  })
-  .then(() => {
+
+    await writeFilePro(`dog-img.txt`, res.body.message);
     console.log(`random image file saved`);
-  })
-  .catch(err => {
+  } catch (err) {
     console.log(err);
-  });
+  }
+};
+
+getDogPic();
+
+// readFilePro(`${__dirname}/dog.txt`)
+//   .then(data => {
+//     console.log(`bread: ${data}`);
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`); //the return will return a promise
+//   })
+//   .then(res => {
+//     console.log(res.body.message);
+//     return writeFilePro(`dog-img.txt`, res.body.message);
+//     // fs.writeFile(`dog-img.txt`, res.body.message, err => {
+//     //   if (err) return console.log(err.message);
+//     //   console.log(`random image file saved`);
+//     // });
+//   })
+//   .then(() => {
+//     console.log(`random image file saved`);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
