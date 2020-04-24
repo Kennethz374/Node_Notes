fa7ff93bb5ef111fc7aab48544e992677730a1e3
Kenneth <Kennethzhang374@gmail.com>
2020-04-23 23:56:58 -0700

    git ignore

--- Asyn_Promises/index.js
+++ Asyn_Promises/index.js
@@ -1,5 +1,18 @@
-const fs = require("fs");
-
+const fs = require('fs');
+const superagent = require('superagent');
+//callback hell
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`bread: ${data}`);
+
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .end((err, res) => {
+      if (err) return console.log(err.message);
+      console.log(res.body.message);
+
+      fs.writeFile(`dog-img.txt`, res.body.message, err => {
+        if (err) return console.log(err.message);
+        console.log(`random image file saved`);
+      });
+    });
 });
